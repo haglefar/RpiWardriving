e8e58ee7a8d65e3f35226f134d8d540f58e43ba6
haglefar <johanbjoerseth@gmail.com>
2025-01-10 18:22:42 +0100

    updates

--- screen.sh
+++ screen.sh
@@ -10,14 +10,12 @@ sudo service NetworkManager stop
 sudo gpsd /dev/ttyUSB0 -F /var/run/gpsd.sock
 #start screen
 screen -dmS status
-screen -S status -X screen watch bash -c /home/pi/status.sh
+screen -S status -X screen watch bash -c /home/req/status.sh
 # hcxdumptool
 screen -dmS wlan1
-screen -S wlan1 -X screen sudo hcxdumptool -i wlan1 -o /home/pi/hashes/wlan1/Wi-Fi_PMKI-wlan1-$(date +"%Y_%m_%d_%I_%M_%p").pcapng --enable_status=3
+screen -S wlan1 -X screen sudo hcxdumptool -i wlx00c0caaaff87 -w /opt/wardriving/hashes/wlx00c0caaaff87/Wi-Fi_PMKI-wlan1-$(date +"%Y_%m_%d_%I_%M_%p").pcapng --rds=3
 screen -dmS wlan2
-screen -S wlan2 -X screen sudo hcxdumptool -i wlan2 -o /home/pi/hashes/wlan2/Wi-Fi_PMKI-wlan2-$(date +"%Y_%m_%d_%I_%M_%p").pcapng --enable_status=3
-screen -dmS wlan3
-screen -S wlan3 -X screen sudo hcxdumptool -i wlan3 -o /home/pi/hashes/wlan3/Wi-Fi_PMKI-wlan3-$(date +"%Y_%m_%d_%I_%M_%p").pcapng --enable_status=3
+screen -S wlan2 -X screen sudo hcxdumptool -i wlx9cefd5fa9b94 -w /opt/wardriving/hashes/wlx9cefd5fa9b94/Wi-Fi_PMKI-wlan2-$(date +"%Y_%m_%d_%I_%M_%p").pcapng --rds=3
 # kismet
 screen -dmS kismet
-screen -S kismet -X screen sudo kismet -c hci0 -c wlan4 -p /home/pi/war-drive
+screen -S kismet -X screen sudo kismet -c hci0 -c wlan1 -p /opt/wardriving/kismetbt
